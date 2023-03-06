ab03a638bcc4371a61b6787c8bb70aadb2ede945
szyper <zskgit@gmail.com>
2023-03-06 13:37:48 +0100

    add new files

---
+++ 2_html.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+	<h4>Lista</h4>
+	<ul>
+		<li>
+			Poznań
+			<ol type="A">
+				<li>Polna</li>
+				<li>Gnieźnieńska</li>
+			</ol>
+		</li>
+		<li>Gniezno</li>
+		<li>Jarocin</li>
+	</ul>
+</body>
+</html>
