242ff44fef6d5a6b32edf8deefa49c85c2cb3aad
szyper <zskgit@gmail.com>
2023-05-16 14:04:51 +0200

    register_user

--- 4_db/scripts/connect.php
+++ 4_db/scripts/connect.php
@@ -1,5 +1,5 @@
 <?php
 //	echo "db";
-	//$conn = new mysqli("localhost", "root", "", "cdv_gr_3");
-	$conn = new mysqli("localhost", "testupdate", "test", "cdv_gr_3");
+	$conn = new mysqli("localhost", "root", "", "cdv_gr_3");
+	//$conn = new mysqli("localhost", "testupdate", "test", "cdv_gr_3");
 //	echo $conn->connect_errno; //0 -> ok
